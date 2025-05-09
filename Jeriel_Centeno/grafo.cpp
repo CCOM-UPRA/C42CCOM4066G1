af96c394cabd6a00c08593aa7e812ffad9dba8b5
JerielCent <Jeriel.centeno@upr.edu>
2025-05-09 15:08:52 -0400

    Update grafo.cpp
    los axis ahora usan vector en ves de arrays normales

--- Jeriel_Centeno/grafo.cpp
+++ Jeriel_Centeno/grafo.cpp
@@ -1,77 +1,70 @@
 #include <iostream>
+#include <vector>
 #include <cstdlib>
+#include <cstring>
 using namespace std;
 
-int main(int argc, char* argv[]) 
-{
-    // Se requieren al menos 9 argumentos
-    if (argc < 9) 
-    {
+// Une los elementos del vector separados por espacio
+string join(const vector<string>& vec) {
+    string result;
+    for (size_t i = 0; i < vec.size(); ++i) {
+        result += vec[i];
+        if (i < vec.size() - 1) result += " ";
+    }
+    return result;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 9) { //validacion
         cerr << "Not enough arguments provided.\n";
         return 1;
     }
 
-    // Directorio de entrada y nombre base del archivo
     string pr_dir = argv[1];
     string k = argv[2];
+    vector<string> x_axis, y_axis, z_axis;
 
-    // Variables para ejes x, y, z
-    string x_axis, y_axis, z_axis;
+    // Reunir x_axis hasta el "|"
     int i = 3;
-
-    // Leer valores para el eje x hasta el delimitador "|"
-    while (i < argc && string(argv[i]) != "|") 
-    {
-        x_axis += argv[i];
-        x_axis += " ";
+    while (i < argc && string(argv[i]) != "|") {
+        x_axis.push_back(argv[i]);
         i++;
     }
-    i++; // Saltar "|"
+    i++; // skip "|"
 
-    // Leer valores para el eje y hasta el siguiente delimitador "|"
-    while (i < argc && string(argv[i]) != "|") 
-    {
-        y_axis += argv[i];
-        y_axis += " ";
+    while (i < argc && string(argv[i]) != "|") {
+        y_axis.push_back(argv[i]);
         i++;
     }
-    i++; // Saltar "|"
+    i++; // skip "|"
 
-    // Leer valores para el eje z hasta que se encuentre "true" o "false"
-    while (i < argc && string(argv[i]) != "true" && string(argv[i]) != "false") 
-    {
-        z_axis += argv[i];
-        z_axis += " ";
+    while (i < argc && string(argv[i]) != "true" && string(argv[i]) != "false") {
+        z_axis.push_back(argv[i]);
         i++;
     }
 
-    // Leer los flags de entrada: 3D, apilable, y nombre del archivo de salida
     string three_d = argv[i++];
     string stackable = argv[i++];
     string output = argv[i++];
 
-    // Construir el comando para ejecutar el script Python
     string cmd = "python3 read_csv.py -i \"" + pr_dir + "/" + k + "mers_F1Scores.csv\"";
-    cmd += " -x " + x_axis;
-    cmd += " -y " + y_axis;
+    cmd += " -x " + join(x_axis);
+    cmd += " -y " + join(y_axis);
 
-    // Si es gráfico 3D, agregar eje z y el flag -d (3D)
-    if (three_d == "true") 
-    {
-        cmd += " -z " + z_axis;
+    if (three_d == "true") {
+        cmd += " -z " + join(z_axis);
         cmd += " -d";
-        if (stackable == "true") 
-            cmd += " -s"; // stack en gráfico 3D
-    } 
-    else if (stackable == "true") 
-        cmd += " -s"; // stack en gráfico 2D
+        if (stackable == "true") cmd += " -s";
+    } else if (stackable == "true") {
+        cmd += " -s";
+    }
 
-    // Agregar el nombre de archivo de salida
     cmd += " -o " + output;
 
-    // Mostrar y ejecutar comando
     cout << "Ejecutando: " << cmd << endl;
     system(cmd.c_str());
 
     return 0;
 }
+
+//################################ SECCION DE JERIEL END #######################
